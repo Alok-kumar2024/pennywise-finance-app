1c1ae5b039f018c6e2ca9a04190bf81d4683825a
Alok-kumar2024 <24je0588@iitism.ac.in>
2026-04-03 05:32:41 +0530

    feat(data): implement finance repository to bridge layers

---
+++ lib/src/data/repositories/finance_repository_imp.dart
@@ -0,0 +1,27 @@
+import 'package:pennywise/src/data/datasources/plaid_remote_data_source.dart';
+import 'package:pennywise/src/domain/entities/transaction_entity.dart';
+import 'package:pennywise/src/domain/repositories/finance_repository.dart';
+
+import '../../domain/entities/account_entity.dart';
+
+class FinanceRepositoryImp implements FinanceRepository {
+  final PlaidRemoteDataSource remoteDataSource;
+
+  FinanceRepositoryImp(this.remoteDataSource);
+
+  @override
+  Future<String> getLinkToken() => remoteDataSource.createLinkToken();
+
+  @override
+  Future<List<TransactionEntity>> getTransactions(String accessToken) async {
+    final models = await remoteDataSource.getTransactions(accessToken);
+    return models;
+  }
+
+  @override
+  Future<List<AccountEntity>> getAccounts(String accessToken) async {
+    final accounts = await remoteDataSource.getAccounts(accessToken);
+
+    return accounts;
+  }
+}
